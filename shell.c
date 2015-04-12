c9d0b8e4722a5ae75598a40d1292a0df98dee7c8
monica gandhi <monicagandhi11@ymail.com>
2015-04-12 17:42:10 -0400

    Update shell.c

--- shell.c
+++ shell.c
@@ -6,14 +6,14 @@ int countToken;
 Cmd cmds[MAX_CMD];
 
 
-
+/*runs when the inital command is passed.*/
 void initCmd(Cmd *cd)
 {
 	cd->numArg=0;
 	cd->isBackGround=NO;
 	cd->reInput=cd->reOutput=cd->reError=-1;
 }
-
+/*runs the command after the inital pass and counts token and parses to yacc file*/
 void init()
 {
 	countToken=countCmd=0;
@@ -21,7 +21,7 @@ void init()
 	for(i=0;i<MAX_CMD;++i)
 		initCmd(&cmds[i]);
 }
-
+/*prints tokens */ 
 void printCmd(Cmd *cd)
 {
 	printf("args  %d : ",cd->numArg);
@@ -38,6 +38,8 @@ void printCmd(Cmd *cd)
 	if(cd->isBackGround)
 		printf("this run in back ground \n");
 }
+/*printing command counts */
+
 void print()
 {
 	int i;
